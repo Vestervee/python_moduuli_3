9c0003784ad6b41b1ef87ef98490412e1c465dc3
vesterveeti <vesteve@hotmail.fi>
2024-01-21 17:31:36 +0200

    tehtävä 2 kohta 2

--- tehtävät.py
+++ tehtävät.py
@@ -1,3 +1,6 @@
-print("hei, Veeti Vesterback")
-käyttäjä = input("mikä sinun nimesi on?")
-print("hyvää päivää" +käyttäjä+"!")
+#tehtävä 2 kohta 2
+import math
+säde_str = input("mikä on ympyrän säde? ")
+säde = float(säde_str)
+pinta = (säde**2 * math.pi)
+print("ympyrän pinta-ala on " + str(pinta))
